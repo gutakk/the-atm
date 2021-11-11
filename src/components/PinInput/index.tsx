1576480a7f5a534cd75e448916f2f5a790983e68
Gut <thiramate@nimblehq.co>
2021-11-11 19:45:29 +0700

    Add handler to pin input

--- src/components/PinInput/index.tsx
+++ src/components/PinInput/index.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const PinInput = (): JSX.Element => {
+  const pinLength: number = 4;
+  const initialPin: string = "";
+  const [pin, setPin] = useState<string>(initialPin);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPin(e.target.value);
+  };
+
+  useEffect(() => {
+    if(pin.length === pinLength) {
+      console.log('GOT 4 PIN');
+      setPin(initialPin);
+    }
+  });
+
   return (
-    <input className="pin-input" type="password" maxLength={4} size={4} />
+    <input
+      className="pin-input"
+      type="password"
+      maxLength={4}
+      size={4}
+      onChange={(e) => handleOnChange(e)}
+      value={pin}
+    />
   );
 };
 
