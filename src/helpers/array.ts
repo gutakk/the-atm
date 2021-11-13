fb1eb6ecdc3e65bf20bf69ecb1525ffe14f3cf8a
Gut <thiramate@nimblehq.co>
2021-11-13 17:31:32 +0700

    Remove helper

--- src/helpers/array.ts
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash';
-
-export const uniquePush = (arr: any[], value: any) => {
-  for(let i = 0; i < arr.length; i++) {
-    if(_.isEqual(arr[i], value)) return
-  }
-  arr.push(value)
-};
