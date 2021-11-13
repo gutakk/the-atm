454527063519b058f83c802da2120c0897c0f36d
Gut <thiramate@nimblehq.co>
2021-11-14 02:44:10 +0700

    Add more test case to atm page cypress

--- cypress/integration/ATM/atm.spec.ts
+++ cypress/integration/ATM/atm.spec.ts
@@ -43,12 +43,13 @@ describe('ATM page', () => {
 
       cy.findByTestId('modalCloseButton').click();
 
+      cy.findByTestId('modal').should('not.exist');
       cy.findByTestId('currentBalance').contains(currentBalance - withdrawAmount);
     });
   });
 
   context('given withdraw amount exceeds balance', () => {
-    it('display warning modal', () => {
+    it('display amount exceeds balance error', () => {
       const withdrawAmount = 900;
 
       cy.intercept('POST', '**/api/pin', (req) => {
@@ -67,6 +68,102 @@ describe('ATM page', () => {
 
       cy.findByTestId('alert').should('be.visible');
       cy.findByTestId('alert').contains('Withdraw amount exceeds balance and overdraft');
+    });
+  });
+
+  context('given withdraw amount overdrawn', () => {
+    const withdrawAmount = 250;
+
+    it('display warning modal and continue to display withdraw successfully modal', () => {
+      cy.intercept('POST', '**/api/pin', (req) => {
+        req.reply({
+          statusCode: 200,
+          body: {
+            "currentBalance": currentBalance
+          }
+        });
+      });
+
+      cy.visit('/');
+      cy.findByTestId('pinInput').type('1111');
+      cy.findByTestId('withdrawInput').type(withdrawAmount.toString());
+      cy.findByTestId('withdrawButton').click();
+
+      cy.findByTestId('modal').should('be.visible');
+      cy.findByTestId('modal').contains('Be careful! You are trying to overdrawn the balance');
+
+      cy.findByTestId('modalContinueButton').click();
+
+      cy.findByTestId('modal').should('be.visible');
+      cy.findByTestId('modal').contains(`Withdraw £${withdrawAmount} successfully`);
+    });
+
+    it('display warning modal, close modal then balance still be the same', () => {
+      const withdrawAmount = 250;
+
+      cy.intercept('POST', '**/api/pin', (req) => {
+        req.reply({
+          statusCode: 200,
+          body: {
+            "currentBalance": currentBalance
+          }
+        });
+      });
+
+      cy.visit('/');
+      cy.findByTestId('pinInput').type('1111');
+      cy.findByTestId('withdrawInput').type(withdrawAmount.toString());
+      cy.findByTestId('withdrawButton').click();
+
+      cy.findByTestId('modal').should('be.visible');
+      cy.findByTestId('modal').contains('Be careful! You are trying to overdrawn the balance');
+
+      cy.findByTestId('modalCloseButton').click();
+
+      cy.findByTestId('modal').should('not.exist');
+      cy.findByTestId('currentBalance').contains(currentBalance);
+    });
+  });
+  
+
+  context('given withdraw amount overdrawn and withdraw with not enough notes', () => {
+    it('display warning modal and continue to display no notes error message', () => {
+      const withdrawAmountFirstRound = 250;
+      const withdrawAmountSecondRound = 35;
+
+      cy.intercept('POST', '**/api/pin', (req) => {
+        req.reply({
+          statusCode: 200,
+          body: {
+            "currentBalance": currentBalance
+          }
+        });
+      });
+
+      cy.visit('/');
+      cy.findByTestId('pinInput').type('1111');
+      cy.findByTestId('withdrawInput').type(withdrawAmountFirstRound.toString());
+      cy.findByTestId('withdrawButton').click();
+
+      cy.findByTestId('modal').should('be.visible');
+      cy.findByTestId('modal').contains('Be careful! You are trying to overdrawn the balance');
+
+      cy.findByTestId('modalContinueButton').click();
+
+      cy.findByTestId('modal').should('be.visible');
+      cy.findByTestId('modal').contains(`Withdraw £${withdrawAmountFirstRound} successfully`);
+
+      cy.findByTestId('modalCloseButton').click();
+      cy.findByTestId('withdrawInput').type(withdrawAmountSecondRound.toString());
+      cy.findByTestId('withdrawButton').click();
+
+      cy.findByTestId('modal').should('be.visible');
+      cy.findByTestId('modal').contains('Be careful! You are trying to overdrawn the balance');
+
+      cy.findByTestId('modalContinueButton').click();
+
+      cy.findByTestId('alert').should('be.visible');
+      cy.findByTestId('alert').contains(`Sorry, we do not have enough notes to withdraw £${withdrawAmountSecondRound}`);
     });
   });
 });
