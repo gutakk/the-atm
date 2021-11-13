53221b06790d4529f58c538e616c97736193c088
Gut <thiramate@nimblehq.co>
2021-11-13 21:07:45 +0700

    Add style to atm screen

--- src/screens/ATM/index.tsx
+++ src/screens/ATM/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useAppSelector } from '../../hooks/useApp';
 import WithdrawForm from './WithdrawForm';
 import WithdrawHandler from './handler';
+import Alert from '../../components/Alert';
 import Modal, { modalType } from '../../components/Modal';
 
 const ATM = (): JSX.Element => {
@@ -23,8 +24,9 @@ const ATM = (): JSX.Element => {
   return (
     <div className="atm-screen page-bg">
       <main className="main-content">
-        <p>Balance: £{currentBalance}</p>
-        {errorMessage && <p>{errorMessage}</p>}
+        <h3 className="main-content__title">BALANCE</h3>
+        <h1 className="main-content__balance"><span className="main-content__balance--currency">£</span>{currentBalance}</h1>
+        {errorMessage && <Alert detail={errorMessage} />}
         {successMessage &&
           <Modal
             isOpen={true}
