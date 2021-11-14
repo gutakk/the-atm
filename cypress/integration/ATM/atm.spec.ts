b1260e48d138b568a9b36be7867b4910f528b31e
Gut <thiramate@nimblehq.co>
2021-11-15 01:28:26 +0700

    Add more cypress test case

--- cypress/integration/ATM/atm.spec.ts
+++ cypress/integration/ATM/atm.spec.ts
@@ -20,7 +20,7 @@ describe('ATM page', () => {
   });
 
   context('given withdraw amount does not exceeds balance', () => {
-    it('display withdraw success modal with bank notes and update current balance', () => {
+    it('displays withdraw success modal with bank notes and update current balance', () => {
       const withdrawAmount = 50;
 
       cy.intercept('POST', '**/api/pin', (req) => {
@@ -49,7 +49,7 @@ describe('ATM page', () => {
   });
 
   context('given withdraw amount exceeds balance', () => {
-    it('display amount exceeds balance error', () => {
+    it('displays amount exceeds balance error', () => {
       const withdrawAmount = 900;
 
       cy.intercept('POST', '**/api/pin', (req) => {
@@ -74,7 +74,7 @@ describe('ATM page', () => {
   context('given withdraw amount overdrawn', () => {
     const withdrawAmount = 250;
 
-    it('display warning modal and comfirm to display withdraw successfully modal', () => {
+    it('displays warning modal and comfirm to display withdraw successfully modal', () => {
       cy.intercept('POST', '**/api/pin', (req) => {
         req.reply({
           statusCode: 200,
@@ -98,7 +98,7 @@ describe('ATM page', () => {
       cy.findByTestId('modal').contains(`Withdraw £${withdrawAmount} successfully`);
     });
 
-    it('display warning modal, close modal then balance still be the same', () => {
+    it('displays warning modal, close modal then balance still be the same', () => {
       const withdrawAmount = 250;
 
       cy.intercept('POST', '**/api/pin', (req) => {
@@ -125,9 +125,8 @@ describe('ATM page', () => {
     });
   });
   
-
   context('given withdraw amount overdrawn and withdraw with not enough notes', () => {
-    it('display warning modal and confirm to display no notes error message', () => {
+    it('displays warning modal and confirm to display no notes error message', () => {
       const withdrawAmountFirstRound = 250;
       const withdrawAmountSecondRound = 35;
 
@@ -164,6 +163,52 @@ describe('ATM page', () => {
 
       cy.findByTestId('alert').should('be.visible');
       cy.findByTestId('alert').contains(`Sorry, we do not have enough notes to withdraw £${withdrawAmountSecondRound}`);
+    });
+  });
+
+  context('given withdraw 140, 50 and 90 in order', () => {
+    it('withdraws success and update correct balance', () => {
+      const withdrawAmount1 = 140;
+      const withdrawAmount2 = 50;
+      const withdrawAmount3 = 90;
+
+      cy.intercept('POST', '**/api/pin', (req) => {
+        req.reply({
+          statusCode: 200,
+          body: {
+            "currentBalance": currentBalance
+          }
+        });
+      });
+
+      cy.visit('/');
+      cy.findByTestId('pinInput').type('1111');
+
+      // Withdraw 140
+      cy.findByTestId('withdrawInput').type(withdrawAmount1.toString());
+      cy.findByTestId('withdrawButton').click();
+      cy.findByTestId('modal').contains(`Withdraw £${withdrawAmount1} successfully`);
+      cy.findByTestId('modal').contains(`£20x4, £10x4, £5x4`);
+      cy.findByTestId('modalCloseButton').click();
+      cy.findByTestId('currentBalance').contains(currentBalance - withdrawAmount1);
+
+      // Withdraw 50
+      cy.findByTestId('withdrawInput').type(withdrawAmount2.toString());
+      cy.findByTestId('withdrawButton').click();
+      cy.findByTestId('modal').contains(`Withdraw £${withdrawAmount2} successfully`);
+      cy.findByTestId('modal').contains(`£20x2, £10x1`);
+      cy.findByTestId('modalCloseButton').click();
+      cy.findByTestId('currentBalance').contains(currentBalance - withdrawAmount1 - withdrawAmount2);
+
+      // Withdraw 90
+      cy.findByTestId('withdrawInput').type(withdrawAmount3.toString());
+      cy.findByTestId('withdrawButton').click();
+      cy.findByTestId('modal').contains('Be careful! You are trying to overdrawn the balance');
+      cy.findByTestId('modalConfirmButton').click();
+      cy.findByTestId('modal').contains(`Withdraw £${withdrawAmount3} successfully`);
+      cy.findByTestId('modal').contains(`£10x9`);
+      cy.findByTestId('modalCloseButton').click();
+      cy.findByTestId('currentBalance').contains(currentBalance - withdrawAmount1 - withdrawAmount2 - withdrawAmount3);
     });
   });
 });
